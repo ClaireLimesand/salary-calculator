6b1f3d3810b17dadc39cc974efada030b87db079
Claire Limesand <clairelimesand@gmial.com>
2021-11-06 12:43:33 -0500

    greated function to add object to array

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 $( document ).ready( onReady )
 
+let employeeList = [
+    {firstName: 'Claire', lastName: 'Placeholder', id: 12345, jobTitle: 'title', annualSalary: 100000},
+];
+
 function onReady(){
-    console.log('wassup')
+    $('#submitButton').on('click', createEmployeeList);
 }
 
-// function 
+function createEmployeeList(listName) {
+    for (let employee of employeeList) {
+        let newTableRow = `
+            <tr>
+                <td>${employee.firstName}</td>
+                <td>${employee.lastName}</td>
+                <td>${employee.id}</td>
+                <td>${employee.jobTitle}</td>
+                <td>${employee.annualSalary}</td>
+            </tr>
+        `;
+        
+    $('#employeeTable').append(newTableRow);
+    }
+}
