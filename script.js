617c0e62da96362f4338ba09dd37ccaf1c8da6ce
Claire Limesand <clairelimesand@gmial.com>
2021-11-07 12:50:29 -0600

    delete button deletes row/does not yet delete object

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 $( document ).ready( onReady )
 
 let employeeList = [];
+let monthlyCost = 0;
 
 function onReady(){
     createEmployeeList(employeeList);
     $('#addEmployee').on('click', handleAddEmployee );
-    renderMonthlyCost(employeeList); 
+    // renderMonthlyCost(employeeList); 
 }
 
 function createEmployeeList(listName) {
@@ -18,25 +19,33 @@ function createEmployeeList(listName) {
                 <td>${employee.id}</td>
                 <td>${employee.jobTitle}</td>
                 <td>${employee.annualSalary}</td>
+                <td>${"<button class='deleteButton'>Delete</button>"}</td>
             </tr>
         `;
         
     $('#employeeTableBody').append(newTableRow);
+    $('.deleteButton').on('click', handleDeleteButton);
     }
 }
 
 function calculateMonthlyCost(employeesToSum) {
     let sum = 0;
     for (let employee of employeesToSum){ 
-        sum =  employee.annualSalary / 12
+        sum +=  employee.annualSalary / 12
     }
+    sum = sum.toFixed(2)
     return sum;
 }
 
 function renderMonthlyCost(employeesToSum) {
-    let monthlyCost = calculateMonthlyCost(employeesToSum)
+    monthlyCost = calculateMonthlyCost(employeesToSum)
     $('#monthlyCost').text(monthlyCost)
-}
+    if (monthlyCost > 20000){
+        $('#turnRed').addClass('turnRed');
+    } else   {
+    $('#turnRed').removeClass('turnRed');
+    }
+};
 
 function handleAddEmployee(){
     let newFirstName = $( '#firstNameInput' ).val();
@@ -65,6 +74,20 @@ function addNewEmployee (newFirstName, newLastName, newId, newTitle, newAnnualSa
         jobTitle: newTitle,
         annualSalary: newAnnualSalary,
     }
-    
     employeeList.push(newEmployee)
 }
+
+function handleDeleteButton() {
+    $(this).closest('tr').remove();
+    renderMonthlyCost();
+
+}
+
+// let index = findIndex
+// $(rowID).remove();
+  //remove employee Object by fed idNumber  
+//   for (let i in array){
+//     if (array[i].idNumber === idNumber ){
+//       array.splice(i, 1);
+//     };
+//   };
